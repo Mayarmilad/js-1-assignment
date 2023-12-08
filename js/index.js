53e35aa45450f37aa49dbc64bdf4c1496015a1f1
mayar <mayarmilad@gmail.com>
2023-12-09 00:28:54 +0200

    js-1-assignment

---
+++ js/index.js
@@ -0,0 +1,353 @@
+/*var num1 = + window.prompt("enter your number");
+console.log(num1);*/
+
+/*==========question2======= */
+/*var num1 =+prompt("enter your number");
+if(num1 %3==0 &&num1%4==0){
+    console.log("true");
+
+}else{console.log("false");}*/
+
+/*==========question3======= */
+/*var x=+prompt("enter yor number 1");
+var y=+prompt("enter yor number 2");
+if(x > y){
+    console.log(x);
+
+}else{
+    console.log(y);
+}*/
+
+/*==========question4======= */
+/*var x=+prompt("enter your number 1");
+if(x>0){
+    console.log("positive");
+
+}else{console.log("negative");}*/
+
+/*==========question5======= */
+
+/*var x = +prompt("enter your first num1");
+var y = +prompt("enter your num2");
+var z = +prompt("enter your num3");
+if (x > y && x > z && y > z) {
+    console.log(x + " max number");
+    console.log(z + " min number");
+} else if (x > y && x > z && z > y) {
+    console.log(x + " max numer");
+    console.log(y + "min number");
+} else if (y > x && y > z && x > z) {
+    console.log(y + " max numer");
+    console.log(z + "min number");
+} else if (y > x && y > z && z > x) {
+    console.log(y + " max numer");
+    console.log(x + "min number");
+}
+else if (z > x && z > y && x > y) {
+    console.log(z + " max number");
+    console.log(y + "min number");
+} else {
+    console.log(z + " max number");
+    console.log(x + "min number");
+}*/
+
+/*==========question6 and 7======= */
+
+/*var num=+prompt("enter your number");
+if(num%2 ==0){
+    console.log("even");
+
+}else{console.log("odd");}*/
+
+/*==========question8======= */
+/*var character = prompt("enter your character");
+if(character=="a" || character =='e' || character== "o" || character== "i"|| character=="u"){
+console.log("vowel");
+}else{
+    console.log("consonant");
+}*/
+
+/*==========question9======= */
+/*var num=+prompt("enter your number");
+for(var i=1;i<=num;i++){
+console.log(i);
+}*/
+
+/*==========question10======= */
+/*var num =+prompt("enter your number");
+for(i=1;i<=12;i++){
+    console.log(i*num);
+}*/
+
+/*==========question11======= */
+/*var num=+prompt("enter your number");
+for(var i=1;i<num;i++){
+console.log(i*2);
+}*/
+
+/*==========question12======= */
+/*var num1=+prompt("enter your num1");
+var num2=+prompt("enter your num2");
+console.log(num1**num2);*/
+
+/*==========question12======= */
+/*var sub1=+prompt("enter your subject1");
+var sub2=+prompt("enter your subject2");
+var sub3=+prompt("enter your subject3");
+var sub4=+prompt("enter your subject4");
+var sub5=+prompt("enter your subject5");
+console.log(sub1+sub2+sub3+sub4+sub5);
+console.log((sub1+sub2+sub3+sub4+sub5)/5);
+console.log((sub1+sub2+sub3+sub4+sub5)/100);*/
+
+/*==========question13======= */
+
+/*var num = +prompt("enter month number");
+if (num == 1) {
+    console.log("31");
+} else if (num == 2) {
+    console.log("28");
+} else if (num == 3) {
+    console.log("30");
+}
+else if (num == 4) {
+    console.log("31");
+}
+else if (num == 5) {
+    console.log("30");
+}
+else if (num == 6) {
+    console.log("31");
+}
+else if (num == 7) {
+    console.log("30");
+}
+else if (num == 8) {
+    console.log("31");
+}
+else if (num == 9) {
+    console.log("30");
+}
+else if (num == 10) {
+    console.log("31");
+}
+else if (num == 11) {
+    console.log("30");
+}
+else  {
+    console.log("31");
+}*/
+
+
+/*==========question14======= */
+/*var sub1=+prompt("mark of chemistry");
+var sub2=+prompt("mark of physcis");
+var sub3=+prompt("mark of mathmathics");
+var sub4=+prompt("mark of computer");
+var sub5=+prompt("mark of biology");
+if(sub1>=90){
+console.log("Grad A");
+}else if(sub1>=80){
+console.log("grad B");
+}else if(sub1>=70){
+console.log("grad C");
+}else if(sub1>=60){
+console.log("grad D");
+}else if(sub1>=40){
+console.log("grad E");
+}else{
+    console.log("grad F");
+}
+if(sub2>=90){
+    console.log("Grad A");
+    }else if(sub2>=80){
+    console.log("grad B");
+    }else if(sub2>=70){
+    console.log("grad C");
+    }else if(sub2>=60){
+    console.log("grad D");
+    }else if(sub2>=40){
+    console.log("grad E");
+    }else{
+        console.log("grad F");
+    }
+    if(sub3>=90){
+        console.log("Grad A");
+        }else if(sub3>=80){
+        console.log("grad B");
+        }else if(sub3>=70){
+        console.log("grad C");
+        }else if(sub3>=60){
+        console.log("grad D");
+        }else if(sub3>=40){
+        console.log("grad E");
+        }else{
+            console.log("grad F");
+        }
+    if(sub4>=90){
+        console.log("Grad A");
+        }else if(sub4>=80){
+        console.log("grad B");
+        }else if(sub4>=70){
+        console.log("grad C");
+        }else if(sub4>=60){
+        console.log("grad D");
+        }else if(sub4>=40){
+        console.log("grad E");
+        }else{
+            console.log("grad F");
+        }
+        if(sub5>=90){
+            console.log("Grad A");
+            }else if(sub5>=80){
+            console.log("grad B");
+            }else if(sub5>=70){
+            console.log("grad C");
+            }else if(sub5>=60){
+            console.log("grad D");
+            }else if(sub5>=40){
+            console.log("grad E");
+            }else{
+                console.log("grad F");
+            }
+           
+*/
+/*==========question15======= */
+/*var days=+prompt("enter month name");
+switch (days) {
+    case "january":
+        console.log("31"); 
+        break;
+        case "February":
+        console.log("28"); 
+        break;
+        case "mars":
+        console.log("30"); 
+        break;
+        case " April":
+        console.log("31"); 
+        break;
+        case "may":
+        console.log("30"); 
+        break;
+        case "June":
+        console.log("31"); 
+        break;
+        case "july":
+        console.log("30"); 
+        break;
+        case "August":
+        console.log("31"); 
+        break;
+        case " September":
+        console.log("30"); 
+        break;
+        case "octaber":
+        console.log("31"); 
+        break;
+        case "November":
+        console.log("30"); 
+        break;
+        
+    default:
+        console.log("31");
+        break;
+}
+*/
+
+/*==========question16======= */
+/*var character = prompt("enter your character");
+switch (character){
+    case "a" :
+        console.log("vowel"); 
+        break;
+
+    case  'e' :
+        console.log("vowel");
+        break;
+
+        case "i" :
+        console.log("vowel"); 
+        break;
+
+    case  'u' :
+        console.log("vowel");
+        break;
+
+        case  'o' :
+        console.log("vowel");
+        break;
+        
+    default:
+        console.log("consonent");
+        break;
+
+}*/
+
+/*==========question17======= */
+/*var num1=+prompt("enter your num1");
+var num2=+prompt("enter your num2");
+
+switch (true) {
+    case num1>num2:
+        console.log("number1 maximum");
+        break;
+        case num1<num2:
+        console.log("number2 maximum");
+        break;
+
+
+    default:
+        console.log("numbers is equal to each other");
+        break;
+}*/
+/*==========question18======= */
+/*var num=+prompt("enter your number");
+switch (true) {
+    case num%2==0:
+        console.log("even");
+        break;
+
+    default:
+        console.log("odd");
+        break;
+}*/
+
+/*==========question19======= */
+/*var num=+prompt("enter your number");
+switch (true) {
+    case num>0:
+        console.log("positive");
+        break;
+
+        case num<0:
+        console.log("negative");
+        break;
+
+    default:
+        console.log("zero");
+        break;
+}*/
+
+/*==========question20======= */
+/*var num1 =+ prompt("enter your num1");
+var calcutate = prompt("what  you want to do")
+var num2 =+ prompt("enter your num2");
+switch (calcutate) {
+    case '+':
+        console.log(num1 + num2);
+        break;
+    case '-':
+        console.log(num1 - num2);
+        break;
+    case '*':
+        console.log(num1 * num2);
+        break;
+    case '/':
+        console.log(num1 / num2);
+        break;
+
+    default:
+        console.log("there is something wrong");
+        break;
+}*/
